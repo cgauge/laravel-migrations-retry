13758e9375bec629937db92098eb3808c75419bb
Deleu <deleugyn@gmail.com>
2018-08-14 07:41:20 -0400

    Include option for seeding the database

--- src/Commands/MigrateRetryCommand.php
+++ src/Commands/MigrateRetryCommand.php
@@ -9,7 +9,8 @@ class MigrateRetryCommand extends BaseCommand
 {
     protected $signature = 'migrate:retry 
         {--tries=5 : The amount of times to retry.}
-        {--delay=3 : The amount in seconds to wait until next retry.}';
+        {--delay=3 : The amount in seconds to wait until next retry.}
+        {--seed   : Whether we should seed the database or not}';
 
     public function handle()
     {
@@ -17,9 +18,25 @@ class MigrateRetryCommand extends BaseCommand
         $delay = (int)$this->option('delay');
 
         retry($tries, function () {
-            Artisan::call('migrate');
+            Artisan::call('migrate', $this->migrateParameters());
         }, $delay * 1000);
 
         $this->output->write(Artisan::output());
+    }
+
+    private function migrateParameters()
+    {
+        $parameters = [];
+
+        if ($this->shouldSeed()) {
+            $parameters['--seed'] = true;
+        }
+
+        return $parameters;
+    }
+
+    private function shouldSeed(): bool
+    {
+        return (bool)$this->option('seed');
     }
 }
